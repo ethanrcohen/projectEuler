b3ccdbce2b020c18d13be997c2ab4ee870c18988
Ethan Cohen <ecohen56@gmail.com>
2019-06-29 22:02:43 -0400

    Add problem 7

---
+++ solutions/007.hs
@@ -0,0 +1,6 @@
+main = print (last(take 10001 primes))
+
+primes = 2 : primes' 
+    where
+        isPrime (p:ps) n = p*p > n ||  n `rem` p /= 0 && isPrime ps n
+        primes' = 3 :  filter (isPrime primes') [5, 7 .. ]
